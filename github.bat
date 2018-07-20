17e1624a78a64d1b6c598fb5578096f0b949ab2e
xiexin <xiexin@canaan-creative.com>
2018-07-20 19:08:38 +0800

    second update

--- github.bat
+++ github.bat
@@ -1,6 +1,6 @@
 git config --global user.name "xiexin"
 REM update email
-git config --global user.email "xiewolf4@163.com"
+git config --global user.email "xiexin@canaan-creative.com"
 git init
 REM update link
 git remote add origin https://github.com/xiexinseu/speech_enhance.git
