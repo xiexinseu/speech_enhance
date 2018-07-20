2f641745cb3cec2b352a9deb7849eee1f922ad63
xiexin <xiewolf4@163.com>
2018-07-20 18:00:38 +0800

    second update

---
+++ github.bat
@@ -0,0 +1,7 @@
+git config --global user.name "xiexin"
+REM update email
+git config --global user.email "xiewolf4@163.com"
+git init
+REM update link
+git remote add origin https://github.com/xiexinseu/speech_enhance.git
+git pull origin master --allow-unrelated-histories
---
+++ gitpush.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "second update"
+git push origin master
